d1b5fb49b52ca64271b44fc58508c51236c31160
Infant Alex <vminfant@gmail.com>
2019-05-01 20:09:00 +0800

    Integer to boolean coversion - Snippet

---
+++ cpp_code_snippets/bool_conv.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+int main() {
+
+        bool b1,b2,b3,b4,b5,b6;
+
+        b1 = -1;
+        b2 = 1;
+        b3 = 0;
+        b4 = 244;
+        b5 = true;
+        b6 = false;
+
+        std::cout << "b1 : " << b1 << "\n";
+        std::cout << "b2 : " << b2 << "\n";
+        std::cout << "b3 : " << b3 << "\n";
+        std::cout << "b4 : " << b4 << "\n";
+        std::cout << "b5 : " << b5 << "\n";
+        std::cout << "b6 : " << b6 << "\n";
+
+
+        return 0;
+}
