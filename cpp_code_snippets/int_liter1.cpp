2b305a3da4e819107fb2ca8743aed375a0bec668
Infant Alex <vminfant@gmail.com>
2019-05-06 21:34:05 +0800

    Integer literal example

---
+++ cpp_code_snippets/int_liter1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main() {
+
+        int i = 2147483648;
+        long l = 9223372036854775808;
+        long long ll = 9223372036854775808;
+
+        std::cout << "i : " << i << "\n";
+        std::cout << "l : " << l << "\n";
+        std::cout << "ll: " << ll << "\n";
+
+        return 0;
+}
